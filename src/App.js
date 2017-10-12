6b1248fceb11bf3cb2f03ce95e1e7596016ec050
Shannon Matloob <smtlb90@gmail.com>
2017-10-12 09:13:06 -0400

    Add SVG class

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import SVG from './SVG';
 
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-
-
-function Sidebar(props) {
-  const headings = props.headings;
-  const listItems = headings.map(heading =>
-    <a href={"#"+heading}>
-      <li>
-        {heading}
-      </li>
-    </a>
-  );
-  return (
-    <ul>{listItems}</ul>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SVG />
+      </div>
+    );
+  }
 }
 
-const headings  = ["Main", "Users", "Analytics", "Report"];
-
-const Main = () => (
-  <Sidebar headings={headings} />
-)
-
-
-const App = () => (
-  <Router>
-    <div>
-      <Route path="/" component={Main}/>
-    </div>
-  </Router>
-)
 export default App
