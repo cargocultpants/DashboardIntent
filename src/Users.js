4085b6a5958b32ae769f1115dfec999bec40b79e
Shannon Matloob <smtlb90@gmail.com>
2017-10-12 12:53:39 -0400

    Add initial Users component

---
+++ src/Users.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Users extends Component {
+  render() {
+    return (
+      <div className="tabs-target" id="users">
+        <div className="row">
+          <div className="col-desktop-12">
+            <section className="o-pane--blue match-heights">
+              <header><span className="o-pane__dot"></span>
+                <h2>Users</h2>
+              </header>
+              <div className="o-pane__contents">
+                <h1></h1>
+                <p></p>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Users;
