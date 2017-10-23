836dda5b1963acd9633466d1a7d1108d3909db85
Shannon Matloob <smtlb90@gmail.com>
2017-10-23 11:22:06 -0400

    Install React component for Chart.js

--- src/Graph.js
+++ src/Graph.js
@@ -1,23 +1,38 @@
-import React, {Component} from 'react';
-import Chart from 'chart.js';
-const myChart = new Chart(ctx, {...});
-
-const ctx = "dashboard";
-
-
-
-
+import React, { Component } from 'react';
+import { scaleBand, scaleLinear } from 'd3-scale';
+import data from './data';
 
 class Graph extends Component {
+  constructor() {
+    super();
+    this.xScale = scaleBand();
+    this.yScale = scaleLinear();
+  }
+
   render() {
+    const margins = { top: 50, right: 20, bottom: 100, left: 60 };
+    const svgDimensions = { width: 800, height: 500 };
+
+    const maxValue = Math.max(...data.map(d => d.value));
+
+    // scaleBand type
+    const xScale = this.xScale
+      .padding(0.5)
+      // scaleBand domain should be an array of specific values
+      // in our case, we want to use movie titles
+      .domain(data.map(d => d.title))
+      .range([margins.left, svgDimensions.width - margins.right])
+
+     // scaleLinear type
+    const yScale = this.yScale
+       // scaleLinear domain required at least two values, min and max
+      .domain([0, maxValue])
+      .range([svgDimensions.height - margins.bottom, margins.top])
+
     return (
-<<<<<<< HEAD
-      <canvas id="dashboard" width="400" height="400"></canvas>
-=======
-      <div>
-        
-      </div>
->>>>>>> b715812df5e5b65b2db53f7541aa35ce1afeb597
+      <svg width={svgDimensions.width} height={svgDimensions.height}>
+         // Bars and Axis comes here
+      </svg>
     );
   }
 }
