4039473ba34b65ffdac1583256638b793157d36d
Shannon Matloob <smtlb90@gmail.com>
2017-10-23 11:56:50 -0400

    Fix ContentView.js error

--- src/ContentView.js
+++ src/ContentView.js
@@ -1,18 +1,10 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
-=======
-import Graph from './Graph'
->>>>>>> b715812df5e5b65b2db53f7541aa35ce1afeb597
 
 class ContentView extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <canvas id="dashboard" width="400" height="400"></canvas>
-=======
-        <Graph />
->>>>>>> b715812df5e5b65b2db53f7541aa35ce1afeb597
       </div>
     );
   }
