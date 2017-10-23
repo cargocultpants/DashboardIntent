d8f05cbba7d046b9d8bfbd2ab9a59a798ef40cf7
Shannon Matloob <smtlb90@gmail.com>
2017-10-23 12:01:47 -0400

    Remove ContentView component from link

--- src/Content.js
+++ src/Content.js
@@ -4,10 +4,12 @@ import ContentView from './ContentView';
 class Content extends Component {
   render() {
     return (
+      <div>
       <a className="o-menu__logo" href="/content">
         <h3>Content</h3>
-        <ContentView />
       </a>
+      <ContentView />
+      </div>
     );
   }
 }
