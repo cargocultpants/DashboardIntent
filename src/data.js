30b707f116f9c70bfaa76fbfe28f7385f63141a4
Shannon Matloob <smtlb90@gmail.com>
2017-10-23 11:44:55 -0400

    Add new packages

--- src/data.js
+++ src/data.js
@@ -1,5 +1,16 @@
-export default [
-  { platform: 'Facebook', score: 42 },
-  { platform: 'Twitter', score: 39 },
-  { platform: 'Instagram', score: 5 }
-]
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [65, 59, 80, 81, 56, 55, 40],
+    }
+  ]
+};
+
+export default data;
